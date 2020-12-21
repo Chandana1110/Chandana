ee15455f187720fdb24da713a798fd7e50f209be
unknown <chandana.m@terralogic.com>
2020-12-21 14:54:25 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, HashRouter, Switch, Route } from "react-router-dom"
+//import "./App.css";
+import page1 from "./components/page1";
+import page2 from "./components/page2";
+import menubar from "./components/menubar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <HashRouter>
+        <Switch>
+          
+          <Route path="/" exact component={menubar}/>
+          <Route path="/page1" component={page1} />
+          <Route path="/page2" component={page2} />
+    
+          {/* <Route component={Error} /> */}
+        </Switch>
+      </HashRouter>
+    </Router>
+  
+        
+        
+    );
+  }
 }
 
 export default App;
